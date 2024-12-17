ba276967ebaaaa3ddc2f38e7ee8f4564a21f4c8e
mateofr67 <145790812+mateofr67@users.noreply.github.com>
2024-12-17 04:23:58 +0100

    Create config.php

---
+++ SQL/config.php
@@ -0,0 +1,8 @@
+<?php
+// config.php
+return [
+    'db_host' => 'localhost',
+    'db_user' => 'root',
+    'db_pass' => 'your_secure_password_here', // Cambia esto por tu contraseña segura
+    'db_name' => '1ccb8097d0e9ce9f154608be60224c7c'
+];
