8bf8fa87a3e062549635f0b3967a0f06245c6301
mateofr67 <145790812+mateofr67@users.noreply.github.com>
2024-12-17 04:34:44 +0100

    Update config.php

--- SQL/config.php
+++ SQL/config.php
@@ -3,6 +3,6 @@
 return [
     'db_host' => 'localhost',
     'db_user' => 'root',
-    'db_pass' => 'your_secure_password_here', // Cambia esto por tu contraseña segura
+    'db_pass' => '', 
     'db_name' => '1ccb8097d0e9ce9f154608be60224c7c'
 ];
